b4af2ca7b81e711641d91fc9703ab640ab3b42ac
Spindelen <erik@manboy.se>
2025-10-16 12:19:38 +0200

    change , to .

--- src/main/java/se/lexicon/ConverterRate.java
+++ src/main/java/se/lexicon/ConverterRate.java
@@ -3,9 +3,9 @@ package se.lexicon;
 public class ConverterRate {
     //Exchange Rate from google 10,59 2025-10-16
     public static double sekToUsd = 0.11;
-    public static double usdToSek = 9,46;
-    public static double sekToEuro = 0,091;
-    public static double euroToSek = 11,03;
+    public static double usdToSek = 9.46;
+    public static double sekToEuro = 0.091;
+    public static double euroToSek = 11.03;
 
 
 }

4e47cc5a8139436906fa2c86a90e5850b6ef31d1
Spindelen <erik@manboy.se>
2025-10-16 11:34:36 +0200

    added converter operations

--- src/main/java/se/lexicon/ConverterOperations.java
+++ src/main/java/se/lexicon/ConverterOperations.java
@@ -1,4 +1,24 @@
 package se.lexicon;
 
 public class ConverterOperations {
+
+    public static double convertsekToUsd(double sekCurrency){
+        double usdCurrency = sekCurrency * ConverterRate.sekToUsd;
+        return usdCurrency;
+        }
+
+    public static double convertusdToSek(double usdCurrency){
+        double sekCurrency = usdCurrency * ConverterRate.usdToSek;
+        return sekCurrency;
+        }
+
+    public static double convertsekToEuro(double sekCurrency){
+        double euroCurrency = sekCurrency * ConverterRate.sekToEuro;
+        return euroCurrency;
+        }
+
+    public static double converteuroToSek(double euroCurrency){
+        double sekCurrency = euroCurrency * ConverterRate.euroToSek;
+        return sekCurrency;
+    }
 }

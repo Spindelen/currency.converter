93e63d7b00803a58804d0acce1ac1e64361d3540
Spindelen <erik@manboy.se>
2025-10-16 09:57:28 +0200

    Added menu display layout

---
+++ src/main/java/se/lexicon/ConverterMenu.java
@@ -0,0 +1,18 @@
+package se.lexicon;
+
+public class ConverterMenu {
+    public static void main(String[] args) {
+
+        System.out.println("Currency Converter");
+        System.out.println("1. Convert SEK to USD");
+        System.out.println("2. Convert USD to SEK");
+        System.out.println("3. Convert SEK to EURO");
+        System.out.println("4. Convert Euro to SEK");
+        System.out.println("0. Exit ");
+        System.out.println(" Enter your choice:");
+
+    }
+
+}
+
+
